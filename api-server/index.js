9a4c1530c3e0003f198004d3bd085f8cce48aa8a
Sergey Opria <seeruyy@gmail.com>
2023-10-11 14:18:34 -0400

    Added nats connector and client

--- api-server/index.js
+++ api-server/index.js
@@ -1,15 +1,54 @@
-// Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
+'use strict'
 
-// Declare a route
-fastify.get('/hello', function handler (request, reply) {
-  reply.send({ hello: 'world' })
-})
+const fastify = require('fastify')
+const {name} = require('./package.json')
+const client = require('../lib/client/index.js')
 
-// Run the server!
-fastify.listen({ host: "0.0.0.0", port: 3000 }, (err) => {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-})
+const port = 3000
+
+module.exports = {
+  start
+}
+
+async function start() {
+  const server = fastify({ logger: true })
+
+  // Declare a route
+  server.get('/hello', function handler (request, reply) {
+    reply.send({ hello: 'world' })
+  })
+
+  await server.listen({
+    port: port
+  , host: '0.0.0.0'
+  })
+
+  const cl = await client()
+}
+
+// Properly teardown on INT and TERM signals.
+process.on('SIGTERM', onSignal)
+process.on('SIGINT', onSignal)
+
+function onSignal(signal) {
+  if (shutting_down) return
+  shutting_down = true
+  log.warn({signal}, 'received signal %s', signal)
+  stop()
+}
+
+/* istanbul ignore next */
+function onError(err) {
+  console.error(err)
+  process.nextTick(() => {
+    throw err
+  })
+}
+
+if (require.main === module) {
+  start()
+    .then(() => {
+      console.log(`successfully started ${name}`, )
+    })
+    .catch(onError)
+}
