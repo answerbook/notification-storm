283271d088b225c49169fb9c9952b33503839f70
stsantilena <12675156+stsantilena@users.noreply.github.com>
2023-10-11 15:10:21 -0400

    Added publish nats function

--- acceptor/acceptor.go
+++ acceptor/acceptor.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/nats-io/nats.go"
 )
 
 func accept(w http.ResponseWriter, req *http.Request) {
@@ -16,12 +20,38 @@ func transform(w http.ResponseWriter, req *http.Request) {
 }
 
 func publish(w http.ResponseWriter, req *http.Request) {
-
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
+	url := os.Getenv("NATS_URL")
+	if url == "" {
+		url = "nats://localhost:14222"
 	}
+
+	nc, _ := nats.Connect(url)
+
+	fmt.Printf("CONNECTION: %v\n", nc)
+
+	defer nc.Drain()
+
+	nc.Publish("greet.joe", []byte("hello"))
+
+	sub, _ := nc.SubscribeSync("greet.*")
+
+	msg, _ := sub.NextMsg(10 * time.Millisecond)
+	fmt.Println("subscribed after a publish...")
+	fmt.Printf("msg is nil? %v\n", msg == nil)
+
+	nc.Publish("greet.joe", []byte("hello"))
+	nc.Publish("greet.pam", []byte("hello"))
+
+	msg, _ = sub.NextMsg(10 * time.Millisecond)
+	fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
+
+	msg, _ = sub.NextMsg(10 * time.Millisecond)
+	fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
+
+	nc.Publish("greet.bob", []byte("hello"))
+
+	msg, _ = sub.NextMsg(10 * time.Millisecond)
+	fmt.Printf("msg data: %q on subject %q\n", string(msg.Data), msg.Subject)
 }
 
 func main() {
